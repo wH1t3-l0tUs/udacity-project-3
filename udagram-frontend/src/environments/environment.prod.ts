3c0e101c6a0816cbdae5761c92da4f3166f12296
TrungBui <trung.bqt97@gmail.com>
2024-05-13 09:16:01 +0700

    re-config public url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: 'http://localhost:8080/api/v0',
+    apiHost:
+        'http://a89d743aab44b4497a77170b120f67e2-1341038008.us-east-2.elb.amazonaws.com:8080/api/v0',
 };
 
 /*
